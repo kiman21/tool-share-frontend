6e48528d37a49d893f8c81752414537b1240139c
kiman21 <119889799+kiman21@users.noreply.github.com>
2023-03-09 14:35:02 -0800

    forms (#10)

---
+++ components/ToolArrangement.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+function ToolArrangementForm(props) {
+    const [toolType, setToolType] = useState('');
+    const [toolName, setToolName] = useState('');
+
+    const handleInputChange = (e) => {
+        const {tool, value} = e.target;
+
+        return tool==='toolName' ? setToolType(value) : setToolName(value);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        alert('Your tool request has been submitted');
+        setToolName('');
+        setToolType('');
+
+    }
+    return (
+        <div>
+          <form className="form">
+            <input
+              value={toolType}
+              name="toolType"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Select Tool Type:"
+            />
+            <input
+              value={toolName}
+              name="toolName"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Tool Description"
+            />
+            <button type="button" onClick={handleFormSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      );
+}
+
+export default ToolArrangementForm;
