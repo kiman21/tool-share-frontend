07739ac91515845ac4d25400d0adb87240008a8f
kiman21 <kim.andrew0121@gmail.com>
2023-03-13 12:56:18 -0700

    front-end-skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
 
 function App() {
     return (
-        <BrowserRouter>
-        <h1>Navbar</h1>
-        <hr/>
-        <Routes>
-            <Route path="/" element={<h1>Sign In</h1>}/>
-            <Route path="/homepage/:id" element={<h1>User Profile</h1>}/>
-            <Route path="/toolform" element={<h1>Submit a Tool</h1>}/>
-            <Route path="/toolarrangement" element={<h1>Borrow a Tool</h1>}/>
-            <Route path="*" element={<h1>404 page not found</h1>}/>
-        </Routes>
-        </BrowserRouter>
-    )
+
+            <BrowserRouter>
+            <Navbar/>
+                <Routes>
+                    <Route path="/signin" element={<h1>Sign In</h1>}/>
+                    <Route path="/profile" element={<h1>User Profile</h1>}/>
+                    <Route path="/toolform" element={<h1>Submit a Tool</h1>}/>
+                    <Route path="/toolarrangement" element={<h1>Borrow a Tool</h1>}/>
+                    <Route path="*" element={<h1>404 page not found</h1>}/>
+                </Routes>
+            </BrowserRouter>
+    );
 }
 
 export default App;
