d23fdbf92909940aa93cf750c2d38f0679542d1a
hermanea <120064373+hermanea@users.noreply.github.com>
2023-03-14 12:43:48 -0700

    added style folder with app.css and added utils folder with api.js (#16)

---
+++ src/utils/API.js
@@ -0,0 +1,117 @@
+TODO: //add heroku url
+const URL_PREFIX = "";
+
+const API = {
+    getUserData:(id, token) =>{
+        return fetch(`${URL_PREFIX}/api/users/${id}`), {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                },
+        }.then(res=>res.json())
+    },
+    isValidToken: (token) => {
+        return fetch(`${URL_PREFIX}/api/users/isValidToken`, {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        }).then((res) => res.json());
+    },
+    getAllUsers: () => {
+        return fetch(`${URL_PREFIX}/api/users`).then((res) => res.json());
+      },
+    login: (loginObj) => {
+        return fetch(`${URL_PREFIX}/api/users/login`, {
+            method: "POST",
+            body: JSON.stringify(loginObj),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => res.json());
+    },
+    signup: (signupObj) => {
+        return fetch(`${URL_PREFIX}/api/users/signup`, {
+            method: "POST",
+            body: JSON.stringify(signupObj),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => res.json());
+    },
+    getAllTools: () => {
+        return fetch(`${URL_PREFIX}/api/tools`).then((res) => res.json());
+    },
+    createTool: (toolObj, token) => {
+        return fetch(`${URL_PREFIX}/api/tools`, {
+            method: "POST",
+            body: JSON.stringify(toolObj),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((res) => res.json());
+    },
+    getToolsByOwner: (token) => {
+        return fetch(`${URL_PREFIX}/api/tools/ownerTools`, {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }).then((res) => res.json());
+      },
+    getAvailableTools: () => {
+        return fetch(`${URL_PREFIX}/api/tools/availableTools`).then((res) => res.json());
+    },
+    getToolsById: (toolid) => {
+        return fetch(`${URL_PREFIX}/api/tools/${toolid}`).then((res) => res.json());
+    },
+    getToolsByType: (toolid) => {
+        return fetch(`${URL_PREFIX}/api/tools/bytype/${toolid}`).then((res) => res.json());
+    },
+    borrowTool: (toolObj, toolid) => {
+        return fetch(`${URL_PREFIX}/api/tools/borrow/${toolid}`, {
+          method: "PUT",
+          body: JSON.stringify(toolObj),
+        }).then((res) => res.json());
+    },
+    returnTool: (toolObj, toolid) => {
+        return fetch(`${URL_PREFIX}/api/tools/return/${toolid}`, {
+          method: "PUT",
+          body: JSON.stringify(toolObj),
+        }).then((res) => res.json());
+    },
+    TODO: // include tool in share request
+    TODO: // delete a tool
+    getUserById: (userid) => {
+        return fetch(`${URL_PREFIX}/api/user/${userid}`).then((res) => res.json());
+    },
+    TODO: // get all user shares
+    createShare: (shareObj, token) => {
+        return fetch(`${URL_PREFIX}/api/shares`, {
+            method: "POST",
+            body: JSON.stringify(shareObj),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((res) => res.json());
+    },
+    getAllShares: () => {
+        return fetch(`${URL_PREFIX}/api/shares`).then((res) => res.json());
+    },
+    TODO: // get shares by id
+    confirmShareRequest: (shareObj, shareid) => {
+        return fetch(`${URL_PREFIX}/api/tools/confirm/${shareid}`, {
+          method: "PUT",
+          body: JSON.stringify(shareObj),
+        }).then((res) => res.json());
+    },
+    denyShareRequest: (shareObj, shareid) => {
+        return fetch(`${URL_PREFIX}/api/tools/deny/${shareid}`, {
+          method: "PUT",
+          body: JSON.stringify(shareObj),
+        }).then((res) => res.json());
+    },
+    
+};
+
+export default API;
