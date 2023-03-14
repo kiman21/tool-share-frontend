3912020400c84e57d64ad07b64a5912fae18d48d
kiman21 <119889799+kiman21@users.noreply.github.com>
2023-03-13 20:18:08 -0700

    more pages (#15)

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ToolForm from "../../components/ToolForm";
+// import "./style.css";
+import API from "../../utils/API";
+import ToolArrangementForm from "../../components/ToolArrangement";
 
-const Profile = () => {
-    return (
+const Profile = (props) => {
+  const params = useParams();
+  console.log(params);
+  const [user, setUser] = useState({});
+  const [isMyPage, setIsMyPage] = useState(false);
+  const fetchUser = () => {
+    API.getUserData(params.id).then((data) => {
+      setUser(data);
+      console.log(props.userId);
+      if (props.userId == params.id) {
+        setIsMyPage(true);
+      } else {
+        setIsMyPage(false);
+      }
+    });
+  };
 
-    )
-}
-
+};
 export default Profile;
