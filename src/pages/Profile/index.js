42eeae13c47a5570827eb50075d4f70923363425
kiman21 <kim.andrew0121@gmail.com>
2023-03-14 20:44:43 -0700

    profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+// import "./style.css";
+import API from "../../utils/API";
+import Toollist from "../../components/Toollist";
 
-const Profile = () => {
+const Profile = (props) => {
+    const params = useParams();
+    console.log(params);
+    const [user, setUser] = useState({});
+    const [isMyPage, setIsMyPage] = useState(false);
+    const fetchUser = () => {
+      API.getUserData(params.id).then((data) => {
+        setUser(data);
+        console.log(props.userId);
+        if (props.userId == params.id) {
+          setIsMyPage(true);
+        } else {
+          setIsMyPage(false);
+        }
+      });
+    };
+    useEffect(() => {
+      fetchUser();
+    }, [props.userId,params.id]);
     return (
-
-    )
-}
+        <div className="Profile">
+            <h1>{user.username} profile:</h1>
+            {isMyPage && <Tool token={props.token} fetchData={fetchUser}  role="create" />}
+        </div>
+    );
+};
 
 export default Profile;
