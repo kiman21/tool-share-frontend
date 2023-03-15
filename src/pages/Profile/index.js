beec11210ce82f75c67928d0c02459099e53efbd
kiman21 <119889799+kiman21@users.noreply.github.com>
2023-03-15 10:30:00 -0700

    profile page (#18)

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,26 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import ToolForm from "../../components/ToolForm";
 // import "./style.css";
 import API from "../../utils/API";
-import ToolArrangementForm from "../../components/ToolArrangement";
+import Toollist from "../../components/Toollist";
 
 const Profile = (props) => {
-  const params = useParams();
-  console.log(params);
-  const [user, setUser] = useState({});
-  const [isMyPage, setIsMyPage] = useState(false);
-  const fetchUser = () => {
-    API.getUserData(params.id).then((data) => {
-      setUser(data);
-      console.log(props.userId);
-      if (props.userId == params.id) {
-        setIsMyPage(true);
-      } else {
-        setIsMyPage(false);
-      }
-    });
-  };
-
+    const params = useParams();
+    console.log(params);
+    const [user, setUser] = useState({});
+    const [isMyPage, setIsMyPage] = useState(false);
+    const fetchUser = () => {
+      API.getUserData(params.id).then((data) => {
+        setUser(data);
+        console.log(props.userId);
+        if (props.userId == params.id) {
+          setIsMyPage(true);
+        } else {
+          setIsMyPage(false);
+        }
+      });
+    };
+    useEffect(() => {
+      fetchUser();
+    }, [props.userId,params.id]);
+    return (
+        <div className="Profile">
+            <h1>{user.username} profile:</h1>
+            {isMyPage && <Tool token={props.token} fetchData={fetchUser}  role="create" />}
+        </div>
+    );
 };
+
 export default Profile;
